d4f863a2c5b7ee4b44e0798e4949abe6f734fbfd
uhiper <umutakbulut1155@hotmail.com>
2024-03-06 22:45:33 +0300

    ccc

---
+++ app.js
@@ -0,0 +1,19 @@
+@keyframes fadeIn {
+    from {
+      opacity: 0;
+      transform: translateY(-20px);
+    }
+    to {
+      opacity: 1;
+      transform: translateY(0);
+    }
+  }
+  
+  .header {
+    animation: fadeIn 1s ease-in-out;
+  }
+  
+  .calculator {
+    animation: fadeIn 1s ease-in-out;
+  }
+  
